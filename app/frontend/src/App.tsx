e48b8cf8d14d2c137b85ac0bfa5a091cb9364256
caribbeanyute <smithcleon@gmail.com>
2025-04-12 18:47:39 -0500

    change recording button to green

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -75,7 +75,7 @@ function App() {
                 <div className="mb-4 flex flex-col items-center justify-center">
                     <Button
                         onClick={onToggleListening}
-                        className={`h-12 w-60 ${isRecording ? "bg-red-600 hover:bg-red-700" : "bg-purple-500 hover:bg-purple-600"}`}
+                        className={`h-12 w-60 ${isRecording ? "bg-red-600 hover:bg-red-700" : "bg-green-600 hover:bg-green-700"}`}
                         aria-label={isRecording ? t("app.stopRecording") : t("app.startRecording")}
                     >
                         {isRecording ? (
