b048c1b4cb974e0d789608c7af03af7d7e8a0576
caribbeanyute <smithcleon@gmail.com>
2025-04-12 18:47:08 -0500

    update app title text to a jamaican color gradient scheme

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -69,7 +69,7 @@ function App() {
                 <img src={logo} alt="Azure logo" className="h-16 w-16" />
             </div>
             <main className="flex flex-grow flex-col items-center justify-center">
-                <h1 className="mb-8 bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-4xl font-bold text-transparent md:text-7xl">
+                <h1 className="mb-8 bg-gradient-to-r from-green-800 via-yellow-400 to-black bg-clip-text text-4xl font-bold text-transparent md:text-7xl">
                     {t("app.title")}
                 </h1>
                 <div className="mb-4 flex flex-col items-center justify-center">
